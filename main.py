8ea33bdc8a184c0c935ae6817eba07839c0640f7
Vik <vikalmeida.dev@gmail.com>
2025-09-23 08:50:53 -0300

    Etapa 1 - Criação do banco e tabela

---
+++ main.py
@@ -0,0 +1,16 @@
+import sqlite3
+import funcao
+
+conexao = sqlite3.connect("biblioteca.db")
+cursor = conexao.cursor()
+
+#Tabela de livros
+cursor.execute("""
+CREATE TABLE IF NOT EXIST livros (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    titulo TEXT NOT NULL,
+    autor TEXT NOT NULL,
+    ano INTEGER
+    disponivel TEXT CHECK(disponivel IN ('Sim', 'Não')) NOT NULL)
+""")
+
