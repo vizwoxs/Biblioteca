630d858a1436b6cfb122f1b63cc33bd607f3b4af
Vik <vikalmeida.dev@gmail.com>
2025-09-23 10:24:58 -0300

    Etapa 3 - Função Listagem de livros

--- funcao.py
+++ funcao.py
@@ -1,9 +1,10 @@
 import sqlite3
 
+
 #Função para inserir os valores
 def inserir_dados(titulo, autor, ano):
-    conexao = sqlite3.connect("biblioteca.db")
-    cursor = conexao.cursor()
+    with sqlite3.connect("biblioteca.db") as conexao:
+        cursor = conexao.cursor
 
     cursor.execute("""
     INSERT INTO livros (titulo, autor, ano)
@@ -11,7 +12,22 @@ def inserir_dados(titulo, autor, ano):
     """, (titulo, autor, ano))
 
     conexao.commit()
-    conexao.close()
 
     print(f"Dados inseridos com sucesso!\nTITULO: {titulo}\nAUTOR: {autor}\nANO: {ano}")
 
+#Função para listar livros 
+def listar_livros(id, titulo, autor, ano, disponivel):
+    with sqlite3.connect("biblioteca.db") as conexao:
+        cursor = conexao.cursor
+    try:
+        for linha in cursor.fetchall():
+            print("-" * 25)
+            print(f"\n10 > LISTA DE LIVROS\nID: {linha[0]} | TÍTULO: {linha[1]} | AUTOR: {linha[2]} | ANO: {linha[3]} | DISPONIBILIDADE: {linha[4]}")
+            print("-" * 25)
+    except sqlite3.OperationalError:
+        print("Não foi possivel acessar o banco de dados, verifique se o arquivo existe ou esta corrompido.")
+    except Exception:
+        print("O banco de dados está ocupado ou inacessível. Por favor, tente novamente mais tarde.")
+
+
+    
