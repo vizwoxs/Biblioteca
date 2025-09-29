138ac2533760a98122ad991f399b05067f8840c0
Vik <vikoliveira.dev@gmail.com>
2025-09-29 11:00:47 -0300

    Etapa 7 - Integração do Streamlit (Criação da pagina de cadastro de livros.)

--- app.py
+++ app.py
@@ -3,4 +3,18 @@ import streamlit as st
 
 st.set_page_config(page_title="Biblioteca SENAI", layout="centered")
 st.title("Biblioteca SENAI")
-menu = st.sidebar("Menu", ["Cadastrar Livro", "Listar Livros", "Atualizar Disponibilidade", "Remover Livro"])
+menu = st.sidebar.radio("Menu", ["Sobre", "Cadastrar Livro", "Listar Livros", "Atualizar Disponibilidade", "Remover Livro"])
+
+#Cadastrar livro
+if menu == "Cadastrar Livro":
+    st.subheader("Cadastrar novo livro")
+    titulo = st.text_input("Titulo do Livro")
+    autor = st.text_input("Autor do livro")
+    ano = st.text_input("Ano de lançamento do livro")
+
+    if st.button("Cadastrar"):
+        st.success("Livro cadastrado com sucesso")
+        if titulo and autor:
+            mensagem = fun.inserir_dados(titulo, autor, ano)
+        else:
+            st.warning("Preencha todos os campos")
